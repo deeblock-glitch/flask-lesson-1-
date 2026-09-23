0e76285910ae1688c15cc734c04cef1417f936dc
Your Name <danielfash2007'gmail.com>
2026-09-23 10:58:20 +0100

    Add base route and template; update home link

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 app.secret_key = "a-long-random-string"
@@ -6,6 +6,10 @@ app.secret_key = "a-long-random-string"
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/base")
+def base():
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
